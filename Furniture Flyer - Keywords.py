d16e5b5c788ade003246d6f3dc9afa8a8a1b98c9
Shithi Maitra <shithi30@gmail.com>
2024-07-19 14:50:32 -0400

    Update Furniture Flyer - Keywords.py

--- Furniture Flyer - Keywords.py
+++ Furniture Flyer - Keywords.py
@@ -1,31 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 # import
 import pandas as pd
 import duckdb
 import re
 
-
-# In[2]:
-
-
 # pref.
 pd.set_option('display.max_rows', 3000)
 
-
-# In[3]:
-
-
 # read
 read_df = pd.read_csv("C:/Users/shith/Downloads/Competitive Landscape - Tepperman's - Furniture - Flyers.csv")
-
-
-# In[4]:
-
 
 # platform cloud
 def word_freq(platform):
@@ -60,19 +45,11 @@ def word_freq(platform):
     # return
     return cloud_df
 
-
-# In[5]:
-
-
 # call
 brick_df = word_freq("The Brick")
 teppermans_df = word_freq("Tepperman''s")
 leons_df = word_freq("Leon''s")
 total_df = word_freq("all")
-
-
-# In[6]:
-
 
 # common - in all
 qry = '''
@@ -87,10 +64,6 @@ order by total_times desc
 '''
 res_df = duckdb.query(qry).df()
 display(res_df)
-
-
-# In[7]:
-
 
 # common - with total
 qry = '''
@@ -109,19 +82,11 @@ order by tbl0.times desc
 res_df = duckdb.query(qry).df()
 display(res_df)
 
-
-# In[8]:
-
-
 # # experiment basis
 # import enchant
 # d = enchant.Dict("en_US")
 # d.check("lrelf")
 # d.suggest("lrelf")
-
-
-# In[ ]:
-
 
 
 
