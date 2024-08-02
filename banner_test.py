9a2822074811852573019da303a29e3ec38ecb51
Shithi Maitra <shithi30@gmail.com>
2024-08-02 10:20:48 -0400

    Update banner_test.py

--- banner_test.py
+++ banner_test.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.service import Service
 import chromedriver_autoinstaller
 import time
 from pyvirtualdisplay import Display
