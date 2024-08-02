3592d6e1d3d7fbcb9093b46471946c21dd2aa70a
Shithi Maitra <shithi30@gmail.com>
2024-08-02 12:22:00 -0400

    Update banner_test.py

--- banner_test.py
+++ banner_test.py
@@ -2,85 +2,49 @@
 from pyvirtualdisplay import Display
 import chromedriver_autoinstaller
 from selenium import webdriver
-import time
+from bs4 import BeautifulSoup
+import smtplib
+from email.mime.text import MIMEText
 
 # setup
-Display(visible = 0, size = (800, 800)).start() 
-chrome_options = webdriver.ChromeOptions().add_argument("ignore-certificate-errors")
+Display(visible = 0, size = (1920, 1080)).start() 
+options = webdriver.ChromeOptions().add_argument("ignore-certificate-errors")
 
 # window
-driver = webdriver.Chrome(options = chrome_options)
+driver = webdriver.Chrome(options = options)
+driver.maximize_window()
 
-# work
-driver.get('http://github.com')
-time.sleep(5)
-driver.close()
+# url
+url = "https://www.canadiantire.ca/en.html"
+driver.get(url)
 
+# soup
+soup_init = BeautifulSoup(driver.page_source, "html.parser")
+soup = soup_init.find("div", attrs = {"class": "regular slick-initialized slick-slider slick-dotted"}).find_all("img")
+len_soup = len(soup)
 
+# banners
+img_links = set()
+for s in soup: 
+    b = s["src"]
+    if "desktop" in b: img_links.add(b)
+        
+# from, to, body
+sender_email = "shithi30@gmail.com"
+recver_email = ["shithi30@outlook.com"]
+body = str(img_links)
 
+# object
+html_msg = MIMEText(body, "html")
+html_msg['Subject'] = "banner_trial"
+html_msg['From'] = "Shithi Maitra"
+html_msg['To'] = ", ".join(recver_email)
 
-
-
-
-# ## import
-# from selenium import webdriver
-# import chromedriver_autoinstaller
-# from bs4 import BeautifulSoup
-# import smtplib
-# from email.mime.text import MIMEText
-
-# chromedriver_autoinstaller.install() 
-
-# ## scrape
-
-# # pref.
-# headers = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"}
-# options = webdriver.ChromeOptions()
-# options.add_argument("no-sandbox")
-# options.add_argument("disable-dev-shm-usage")
-# options.add_argument("ignore-certificate-errors")
-
-# # window
-# driver = webdriver.Chrome(options = options)
-# driver.maximize_window()
-
-# ## Canadian Tire
-
-# # url
-# url = "https://www.canadiantire.ca/en.html"
-# driver.get(url)
-
-# # soup
-# soup_init = BeautifulSoup(driver.page_source, "html.parser")
-# soup = soup_init.find("div", attrs = {"class": "regular slick-initialized slick-slider slick-dotted"}).find_all("img")
-# len_soup = len(soup)
-
-# # banners
-# img_links = set()
-# for s in soup: 
-#     b = s["src"]
-#     if "desktop" in b: img_links.add(b)
-
-# ## email
-
-# # summary
-
-# # from, to, body
-# sender_email = "shithi30@gmail.com"
-# recver_email = ["shithi30@outlook.com"]
-# body = str(img_links)
-
-# # object
-# html_msg = MIMEText(body, "html")
-# html_msg['Subject'] = "banner_trial"
-# html_msg['From'] = "Shithi Maitra"
-# html_msg['To'] = ", ".join(recver_email)
-
-# # send
-# with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-#    server.login(sender_email, "uhfu cppa sxgh bwpr")
-#    server.sendmail(sender_email, recver_email, html_msg.as_string())
+# send
+with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+   server.login(sender_email, "uhfu cppa sxgh bwpr")
+   server.sendmail(sender_email, recver_email, html_msg.as_string())
     
-# ## end
-# driver.close()
+# end
+driver.close()
 
