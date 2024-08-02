eb87b0d3ce71a138ab0bb80888de9a24d6756f82
Shithi Maitra <shithi30@gmail.com>
2024-08-02 09:59:30 -0400

    Update banner_test.py

--- banner_test.py
+++ banner_test.py
@@ -13,32 +13,17 @@ chromedriver_autoinstaller.install()  # Check if the current version of chromedr
                                       # then add chromedriver to path
 
 chrome_options = webdriver.ChromeOptions()    
-# Add your options as needed    
 options = [
-  # Define window size here
    "--window-size=1200,1200",
     "--ignore-certificate-errors"
- 
-    #"--headless",
-    #"--disable-gpu",
-    #"--window-size=1920,1200",
-    #"--ignore-certificate-errors",
-    #"--disable-extensions",
-    #"--no-sandbox",
-    #"--disable-dev-shm-usage",
-    #'--remote-debugging-port=9222'
 ]
 
-for option in options:
-    chrome_options.add_argument(option)
+for option in options: chrome_options.add_argument(option)
 
     
 driver = webdriver.Chrome(options = chrome_options)
-
 driver.get('http://github.com')
-
 time.sleep(5)
-
 driver.close()
 
 
